6798522c4db33ee70afa5c55f0254bdbbfc2446e
cromagnoli <cristianromagnoli@gmail.com>
2026-02-18 00:45:44 -0300

    Delete .docusaurus

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/cristian/Downloads/cromagnoli-portfolio/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/cristian/Downloads/cromagnoli-portfolio/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/cristian/Downloads/cromagnoli-portfolio/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/cristian/Downloads/cromagnoli-portfolio/src/css/custom.css"),
-];
